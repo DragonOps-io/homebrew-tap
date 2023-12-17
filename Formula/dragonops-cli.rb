4ca9ff3eadf4f291fbbe65eca3e4b6309cf665be
Matt Martinez <matthewmartinez1003@gmail.com>
2023-12-17 17:43:02 -0600

    commit

--- Formula/dragonops-cli.rb
+++ Formula/dragonops-cli.rb
@@ -1,1 +1,11 @@
+class DragonopsCli < Formula
+  desc "This is the DragonOps CLI"
+  homepage "https://github.com/DragonOps-io/homebrew-tap"
+  url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.1-darwin-amd64.zip"
+  sha256 "558022c538f0fb08c56a71b1b8ad4d3e2883c8c1e53a902d2b868918a14423c8"
+  license ""
 
+  test do
+    system "#{bin}/dragonops-cli", "--version"
+  end
+end
