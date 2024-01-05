55a6b3d6bffdd88fa557bd93c9acab5db57372e9
kia martinez <kiawn@46.qarestr.sub-172-16-0.myvzw.com>
2024-01-05 09:06:43 -0800

    new version 0.0.4

--- Formula/dragonops-cli.rb
+++ Formula/dragonops-cli.rb
@@ -3,12 +3,12 @@ class DragonopsCli < Formula
   homepage "https://github.com/DragonOps-io/homebrew-tap"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.3-darwin-amd64.zip"
-    sha256 "14bc177cfb290cc2774e70004a39c5ab84694b165318143309667f51cb6ee7e1"
+    url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.4-darwin-amd64.zip"
+    sha256 "644b5878af780a93c7f2c29b5c587c50ea2da04526f56b1284fd710fdf3f3e0c"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.3-darwin-arm64.zip"
-    sha256 "65dbbfbdf45d85bed0a0db7363cc2705605cdf3a13f0e632806eb58e80af58e3"
+    url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.4-darwin-arm64.zip"
+    sha256 "0b76d4cd538a9fba723cce23de077904308d2a62c2a2afce20f2e4535561093f"
   end
   def install
     bin.install "dragonops-cli"
