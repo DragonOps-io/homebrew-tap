0ee093af48225ee809e3fdd3130ab35d3d3f07a4
kia martinez <kiawn@MacBook-Pro-3.local>
2024-01-08 19:11:48 -0700

    version 0.0.8

--- Formula/dragonops-cli.rb
+++ Formula/dragonops-cli.rb
@@ -4,11 +4,11 @@ class DragonopsCli < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.7-darwin-amd64.zip"
-    sha256 "d816c43766848dd5871fcda91ef25292a3affa0ec4a97157f533f32841ec322f"
+    sha256 "bb87233d00c6fe9ad5302759a54edcb590489745793a122770656fe4d72afd80"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/dragonops-cli_0.0.7-darwin-arm64.zip"
-    sha256 "58c663b9064a2a8834f6ed561ec3ed3dbffd754973fe8f71819a97d2aa48a768"
+    sha256 "63cca6e19fc3d7ffcd2f84899b3229e83cc0ce9059a8f4b6856ca8c6f2162b00"
   end
   def install
     bin.install "dragonops-cli"
