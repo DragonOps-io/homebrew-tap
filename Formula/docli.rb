7991bdad459fbe764e7b6c56c41d19dd2e147a6e
kiawnna <kiawnna@gmail.com>
2024-03-30 12:28:31 -0500

    Brew formula update for docli version v0.0.27

--- Formula/docli.rb
+++ Formula/docli.rb
@@ -5,20 +5,20 @@
 class Docli < Formula
   desc "DragonOps CLI to deploy and manage cloud infrastructure"
   homepage "https://github.com/DragonOps-io/homebrew-tap"
-  version "0.0.26"
+  version "0.0.27"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.26/docli_Darwin_x86_64.tar.gz"
-      sha256 "2f0eeaa90e758d759cf02244c8a47829bffbc231a0a25c20a755c6141950caf4"
+      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.27/docli_Darwin_x86_64.tar.gz"
+      sha256 "543842a7cbf7475dd2dec3053d5635025df643faacd225df47634db17c31dff2"
 
       def install
         bin.install "docli"
       end
     end
     if Hardware::CPU.arm?
-      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.26/docli_Darwin_arm64.tar.gz"
-      sha256 "b2e927396de46b812599623409f3eb975186f03b1f9768a748ecbce76bba2640"
+      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.27/docli_Darwin_arm64.tar.gz"
+      sha256 "550b578a2a48375c659aed746e2fef9343bf22fd1b8598ac0bb3be219f077ac4"
 
       def install
         bin.install "docli"
@@ -27,25 +27,25 @@ class Docli < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.26/docli_Linux_armv6.tar.gz"
-      sha256 "31c8a374ef705829bad4420d3f691938a8ae14f67659e118c67d9c0512c82350"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.27/docli_Linux_arm64.tar.gz"
+      sha256 "e2536da95156153c6862e0bedce35b491f30c08f68c7a515ebcd25be2f9196e8"
 
       def install
         bin.install "docli"
       end
     end
     if Hardware::CPU.intel?
-      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.26/docli_Linux_x86_64.tar.gz"
-      sha256 "a3b7d5edd71eaa903f0fd77d81c9f8fcacd8db4e31c96158249587aa159162e8"
+      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.27/docli_Linux_x86_64.tar.gz"
+      sha256 "dc04a9e0682d3056081bf2f17bb37d21a559a8862b4967b84c73d23a07e4e9da"
 
       def install
         bin.install "docli"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.26/docli_Linux_arm64.tar.gz"
-      sha256 "9f0197ae7adbc22d864242d39ef6621dc3e445e17271336025330bb3cfa01d46"
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://dragonops-cli-releases.s3.amazonaws.com/dragonops-cli/0.0.27/docli_Linux_armv6.tar.gz"
+      sha256 "a9f3672e4f6986bed9f725086b4847a173cb2781356b2a24d433f60e4463b3e2"
 
       def install
         bin.install "docli"
